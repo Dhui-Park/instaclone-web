dcf483d38bbf9617859c8531d0a482971e3b0138
Dhui-Park <hasong925@gmail.com>
2022-12-05 15:42:41 +0900

    Cleaning Code Log In

--- instaclone-web/src/styles.js
+++ instaclone-web/src/styles.js
@@ -1,9 +1,10 @@
+import { faRightToBracket } from "@fortawesome/free-solid-svg-icons";
 import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 
 export const lightTheme = {
-  fontColor: "#2c2c2c",
-  bgColor: "white",
+  accent: "#0095f6",
+  borderColor: "rgb(219,219,219)",
 };
 
 export const darkTheme = {
@@ -20,11 +21,12 @@ export const GlobalStyles = createGlobalStyle`
       box-sizing:border-box;
     }
   body {
-    background-color: #FAFAFA;
-        font-size:14px;
-        font-family:'Open Sans', sans-serif;
+      background-color: #FAFAFA;
+      font-size:14px;
+      font-family:'Open Sans', sans-serif;
+      color: rgb(38,38,38);
     }
     a {
       text-decoration: none;
-  }
+    }
 `;
